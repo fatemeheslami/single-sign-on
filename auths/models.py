7bfc07ce22df18057fafaf74469290033f13e5d3
fatemeheslami <fatemehjadid79@gmail.com>
2022-08-10 21:55:33 +0430

    add random token for request token

--- auths/models.py
+++ auths/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+import random
+from django.utils.crypto import get_random_string
+
+def my_function():
+  return get_random_string(length = random.randint(20,25))
 
 
 # Create your models here.
 class Authentication(models.Model):
-    request_token = models.CharField(max_length=25 , unique=True)
+    request_token = models.CharField(max_length=25 , unique=True , default = my_function)
     auth_token = models.CharField(max_length=25 , unique=True , blank = True , null=True)
     redirect_url = models.URLField(max_length=200)
     user = models.ForeignKey(User , on_delete=models.CASCADE ,  blank = True , null=True , related_name="authentications")
