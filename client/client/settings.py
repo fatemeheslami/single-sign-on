54327626517e1b93cf58299a361972ac795b09f5
fatemeheslami <fatemehjadid79@gmail.com>
2022-09-02 13:49:37 +0430

    add middleware

--- client/client/settings.py
+++ client/client/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'users.middleware.CheckAuthTokenMiddleware'
 ]
 
 ROOT_URLCONF = 'client.urls'
